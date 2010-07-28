4eefe15feedfdf10717416774f9c3600af4b897f
Parker Phinney <parker.phinney@gmail.com>
2010-07-27 23:43:49 -0400

    removed print statement as well as big block of commented-out reference code

--- mysite/mainapp/views.py
+++ mysite/mainapp/views.py
@@ -35,7 +35,6 @@ def view_image(request, image__pk):
     image.update(image_tuples)
 
     if not image['url_to_lores_img']:
-        print "ohboy"
         referrer = request.META.get('HTTP_REFERER')
         if not referrer.find('/view/') == (-1):
             prev_id = int(referrer.rstrip('/').rsplit('/', 1)[1])
@@ -103,71 +102,3 @@ def view_image(request, image__pk):
     image['prev_id'] = int(image['id']) - 1
 
     return render_to_response('image.html', {'image': image, 'image_urls': image_urls})
-
-
-
-'''
-def get_user_or_create(ip_address):
-    try:
-        user = real_time_voting.mainapp.models.User.objects.get(ip_address=ip_address)
-    except real_time_voting.mainapp.models.User.DoesNotExist:
-        print "making a new user" # NOTE: DEBUG
-        # make a new user
-        user = real_time_voting.mainapp.models.User(ip_address=ip_address)
-        user.save()
-    return user
-        
-
-def main(request):
-    # get all events from db
-    events = real_time_voting.mainapp.models.Event.objects.all()
-    
-    return render_to_response('index.html', {'events': events})
-
-
-def create_event(request):
-    return render_to_response('create_event.html', {})
-
-def view_results(request, event__pk):
-    votes = real_time_voting.mainapp.models.Vote.objects.order_by('timestamp').filter(timestamp__isnull=False).filter(event__pk=event__pk).reverse()
-    event = real_time_voting.mainapp.models.Event.objects.get(pk=event__pk)
-    return render_to_response('results.html', {'successful_vote': True, 'votes': votes, 'event': event})
-
-def create_event_do(request):
-    name = request.POST['name']
-    description = request.POST['description']
-    newevent = real_time_voting.mainapp.models.Event(name=name, description=description)
-    newevent.save()
-    #url_to_redirect_to = reverse(real_time_voting.mainapp.views.event) + "/" + newevent.pk
-    url_to_redirect_to = "event/" + str(newevent.pk)
-    return HttpResponseRedirect(url_to_redirect_to)
-
-def event(request, event__pk):
-    event = real_time_voting.mainapp.models.Event.objects.get(pk=event__pk)
-    user = get_user_or_create(request.META.get('REMOTE_ADDR'))
-    her_num_votes = len(real_time_voting.mainapp.models.Vote.objects.all().filter(user=user))
-    return render_to_response('event.html', {'event': event, 'user': user, 'her_num_votes': her_num_votes})
-
-def process_vote_do(request):
-    weight = request.POST['weight']
-    event = real_time_voting.mainapp.models.Event.objects.get(pk=request.POST['event__pk'])
-    user = get_user_or_create(request.META.get('REMOTE_ADDR'))
-    newvote = real_time_voting.mainapp.models.Vote(weight=weight, timestamp=datetime.datetime.now(), event=event, user = user)
-    newvote.save()
-
-    #url_to_redirect_to = reverse(real_time_voting.mainapp.views.vote_success)
-    url_to_redirect_to = "event/" + str(event.pk)
-    return HttpResponseRedirect(url_to_redirect_to)
-
-def process_user_do(request):
-    user = get_user_or_create(request.META.get('REMOTE_ADDR'))
-    user.name = request.POST['name']
-    user.age = request.POST['age']
-    user.gender = request.POST['gender']
-    user.save()
-    from_event__pk = request.POST['from_event__pk']
-
-#    url_to_redirect_to = reverse(real_time_voting.mainapp.views.event)
-    url_to_redirect_to = "/event/%s" % from_event__pk
-    return HttpResponseRedirect(url_to_redirect_to)
-    '''
